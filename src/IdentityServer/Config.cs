848b716a7814b849deba2e0a4ad0764f0ad2fa10
jehanperera <jehan.k.perera@gmail.com>
2025-03-23 16:04:24 +0800

    Add IS configs and clients

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -6,15 +6,26 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
+        {
             new IdentityResources.OpenId()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            { };
+            {
+                new ApiScope(name: "api1", displayName: "My API")
+            };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        new Client[]
+            {
+                new Client{
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials, // no interactive user, use the clientid/secret for authentication
+                    ClientSecrets={ // secret for authentication
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={ "api1" } // scopes that client has access to
+                }
+            };
 }
