f343beff615d7a96ed7f8ddab592c526e86715a7
Jehan Perera <jehan.k.perera@gmail.com>
2025-04-10 22:33:45 +0800

    Add Token Management

--- src/WebClient/Pages/CallApi.cshtml.cs
+++ src/WebClient/Pages/CallApi.cshtml.cs
@@ -1,20 +1,20 @@
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Duende.IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
-    public class CallApiModel : PageModel
+    public class CallApiModel(IHttpClientFactory httpClientFactory) : PageModel
     {
         public string Json = string.Empty;
         public async Task OnGet()
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization=new AuthenticationHeaderValue("Bearer", accessToken);
+            var client = httpClientFactory.CreateClient("apiClient");
+
             var content = await client.GetStringAsync("https://localhost:6001/identity");
 
             var parsed = JsonDocument.Parse(content);
--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddOpenIdConnectAccessTokenManagement();
+builder.Services.AddUserAccessTokenHttpClient("apiClient", configureClient: client =>{
+    client.BaseAddress=new Uri("https://localhost:6001");
+});
+
 builder.Services.AddAuthentication(options=>{
     options.DefaultScheme="Cookies";
     options.DefaultChallengeScheme="oidc";
@@ -22,6 +27,7 @@ builder.Services.AddAuthentication(options=>{
     options.Scope.Add("profile");
     options.Scope.Add("verification");
     options.Scope.Add("api1");
+    options.Scope.Add("offline_access");
     options.ClaimActions.MapJsonKey("email_verified", "email_verified");
     options.GetClaimsFromUserInfoEndpoint=true;
 
