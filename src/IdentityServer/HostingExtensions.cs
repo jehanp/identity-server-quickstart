156471d7e98a1f3a623c1c00cce43bd9f1d8492c
Jehan Perera <jehan.k.perera@gmail.com>
2025-04-12 18:38:36 +0800

    Add EF Core Configuation And Operational Data

--- src/IdentityServer/HostingExtensions.cs
+++ src/IdentityServer/HostingExtensions.cs
@@ -1,6 +1,9 @@
 using Duende.IdentityServer;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
 using Duende.IdentityServer.Test;
 using Google.Apis.Auth.AspNetCore3;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
@@ -13,14 +16,24 @@ internal static class HostingExtensions
         // uncomment if you want to add a UI
         builder.Services.AddRazorPages();
 
+        var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+        // const string connectionString = @"Data Source=Duende.IdentityServer.Quickstart.EntityFramework.db";
+        var connectionString = builder.Configuration["ConnectionStrings:Rds"];
+
         builder.Services.AddIdentityServer(options =>
             {
                 // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
                 options.EmitStaticAudienceClaim = true;
             })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
+            // .AddInMemoryIdentityResources(Config.IdentityResources)
+            // .AddInMemoryApiScopes(Config.ApiScopes)
+            // .AddInMemoryClients(Config.Clients)
+            .AddConfigurationStore(options=>{
+                options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options=>{
+                options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
             .AddTestUsers(TestUsers.Users);
 
         builder.Services.AddAuthentication()
@@ -63,6 +76,8 @@ internal static class HostingExtensions
             app.UseDeveloperExceptionPage();
         }
 
+        // InitializeDatabase(app);
+
         // uncomment if you want to add a UI
         app.UseStaticFiles();
         app.UseRouting();
@@ -74,5 +89,34 @@ internal static class HostingExtensions
         app.MapRazorPages().RequireAuthorization();
 
         return app;
+    }
+
+    private static void InitializeDatabase(IApplicationBuilder app){
+        using(var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope()){
+            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
+            if(!context.Clients.Any()){
+                foreach(var client in Config.Clients){
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+            
+            if(!context.IdentityResources.Any()){
+                foreach(var resource in Config.IdentityResources){
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if(!context.ApiScopes.Any()){
+                foreach(var scope in Config.ApiScopes){
+                    context.ApiScopes.Add(scope.ToEntity());
+                }
+                context.SaveChanges();
+            }
+        }
     }
 }
