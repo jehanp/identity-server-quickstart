28de0fcc62b6672dd3451470f0ed3064e3177c51
Jehan Perera <jehan.k.perera@gmail.com>
2025-04-20 11:55:31 +0800

    Remove Inmemory Configs

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -6,50 +6,50 @@ namespace IdentityServer;
 
 public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Profile(),
-            new IdentityResource(){
-                Name="verification",
-                UserClaims={
-                    JwtClaimTypes.Email,
-                    JwtClaimTypes.EmailVerified,
-                },
-            },
-        };
+    // public static IEnumerable<IdentityResource> IdentityResources =>
+    //     new IdentityResource[]
+    //     {
+    //         new IdentityResources.OpenId(),
+    //         new IdentityResources.Profile(),
+    //         new IdentityResource(){
+    //             Name="verification",
+    //             UserClaims={
+    //                 JwtClaimTypes.Email,
+    //                 JwtClaimTypes.EmailVerified,
+    //             },
+    //         },
+    //     };
 
-    public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-            {
-                new ApiScope(name: "api1", displayName: "My API")
-            };
+    // public static IEnumerable<ApiScope> ApiScopes =>
+    //     new ApiScope[]
+    //         {
+    //             new ApiScope(name: "api1", displayName: "My API")
+    //         };
 
-    public static IEnumerable<Client> Clients =>
-        new Client[]
-            {
-                new Client{
-                    ClientId="client",
-                    AllowedGrantTypes=GrantTypes.ClientCredentials, // no interactive user, use the clientid/secret for authentication
-                    ClientSecrets={ // secret for authentication
-                        new Secret("secret".Sha256())
-                    },
-                    AllowedScopes={ "api1" } // scopes that client has access to
-                },
-                new Client{
-                    ClientId="web",
-                    ClientSecrets= { new Secret("secret".Sha256())},
-                    AllowedGrantTypes=GrantTypes.Code,
-                    RedirectUris={"https://localhost:5002/signin-oidc"},
-                    PostLogoutRedirectUris={"https://localhost:5002/signout-callback-oidc"},
-                    AllowOfflineAccess=true,
-                    AllowedScopes={
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "verification",
-                        "api1"
-                    }
-                }
-            };
+    // public static IEnumerable<Client> Clients =>
+    //     new Client[]
+    //         {
+    //             new Client{
+    //                 ClientId="client",
+    //                 AllowedGrantTypes=GrantTypes.ClientCredentials, // no interactive user, use the clientid/secret for authentication
+    //                 ClientSecrets={ // secret for authentication
+    //                     new Secret("secret".Sha256())
+    //                 },
+    //                 AllowedScopes={ "api1" } // scopes that client has access to
+    //             },
+    //             new Client{
+    //                 ClientId="web",
+    //                 ClientSecrets= { new Secret("secret".Sha256())},
+    //                 AllowedGrantTypes=GrantTypes.Code,
+    //                 RedirectUris={"https://localhost:5002/signin-oidc"},
+    //                 PostLogoutRedirectUris={"https://localhost:5002/signout-callback-oidc"},
+    //                 AllowOfflineAccess=true,
+    //                 AllowedScopes={
+    //                     IdentityServerConstants.StandardScopes.OpenId,
+    //                     IdentityServerConstants.StandardScopes.Profile,
+    //                     "verification",
+    //                     "api1"
+    //                 }
+    //             }
+    //         };
 }
--- src/IdentityServer/HostingExtensions.cs
+++ src/IdentityServer/HostingExtensions.cs
@@ -91,32 +91,32 @@ internal static class HostingExtensions
         return app;
     }
 
-    private static void InitializeDatabase(IApplicationBuilder app){
-        using(var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope()){
-            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+    // private static void InitializeDatabase(IApplicationBuilder app){
+    //     using(var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope()){
+    //         serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
 
-            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-            context.Database.Migrate();
-            if(!context.Clients.Any()){
-                foreach(var client in Config.Clients){
-                    context.Clients.Add(client.ToEntity());
-                }
-                context.SaveChanges();
-            }
+    //         var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+    //         context.Database.Migrate();
+    //         if(!context.Clients.Any()){
+    //             foreach(var client in Config.Clients){
+    //                 context.Clients.Add(client.ToEntity());
+    //             }
+    //             context.SaveChanges();
+    //         }
             
-            if(!context.IdentityResources.Any()){
-                foreach(var resource in Config.IdentityResources){
-                    context.IdentityResources.Add(resource.ToEntity());
-                }
-                context.SaveChanges();
-            }
+    //         if(!context.IdentityResources.Any()){
+    //             foreach(var resource in Config.IdentityResources){
+    //                 context.IdentityResources.Add(resource.ToEntity());
+    //             }
+    //             context.SaveChanges();
+    //         }
 
-            if(!context.ApiScopes.Any()){
-                foreach(var scope in Config.ApiScopes){
-                    context.ApiScopes.Add(scope.ToEntity());
-                }
-                context.SaveChanges();
-            }
-        }
-    }
+    //         if(!context.ApiScopes.Any()){
+    //             foreach(var scope in Config.ApiScopes){
+    //                 context.ApiScopes.Add(scope.ToEntity());
+    //             }
+    //             context.SaveChanges();
+    //         }
+    //     }
+    // }
 }
